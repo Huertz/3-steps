9fd4a1c356709fb383f79b1d41988d6309e1557f
Huertz <104065687+Huertz@users.noreply.github.com>
2023-12-02 10:22:02 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className='steps'>
+      <div className='numbers'>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+
+        <p className='meseege'>Hello </p>
+      </div>
+    </div>
+  );
+}
