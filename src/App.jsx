b9f951812dbab4c706656af9927a08fdd9710ac4
Huertz <104065687+Huertz@users.noreply.github.com>
2023-12-02 11:46:12 -0600

    updating state bs on cur val

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,12 @@ const messages = [
 export default function App() {
   const [step, setStep] = useState(1);
   function handlePrevious() {
-    if (step > 1) setStep(step - 1);
+    // if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((s) => s - 1);
   }
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    // if (step < 3) setStep(step + 1);
+    if (step > 1) setStep((s) => s + 1);
   }
 
   return (
